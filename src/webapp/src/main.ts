2e3257ce211c224cffe91351403e9d2ac6c9c610
Davide Vernizzi <davide@vernizzis.it>
2021-01-25 17:53:57 +0100

    ci: update linter

--- src/webapp/src/main.ts
+++ src/webapp/src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+/* eslint-disable */
+async function bootstrap () {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
+/* eslint-enable */
