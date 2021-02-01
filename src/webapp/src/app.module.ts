3be6307b391a93ed5970e05b5e10d1e5ddf5973b
Davide Vernizzi <davide@vernizzis.it>
2021-02-01 10:06:04 +0100

    style(): linted

--- src/webapp/src/app.module.ts
+++ src/webapp/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ApiModule } from './api/api.module'
