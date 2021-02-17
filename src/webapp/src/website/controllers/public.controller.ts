ce9c605b52fb74f3a4b9415a2e531980a99aab4b
Emanuele Cesena <emanuele.cesena@gmail.com>
2021-02-16 21:42:41 -0800

    Add generic pages as md

--- src/webapp/src/website/controllers/public.controller.ts
+++ src/webapp/src/website/controllers/public.controller.ts
@@ -3,12 +3,20 @@ import {
   Get,
   Request,
   Res,
-  UseGuards
+  UseGuards,
+  NotFoundException
   //   Param
 } from '@nestjs/common'
 import { Response } from 'express'
+import { Liquid } from 'liquidjs'
+import * as MarkdownIt from 'markdown-it'
+
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import * as yaml from 'js-yaml'
+
 import { SettingsService } from '../../settings/settings.service'
-import { UserOptionalAuthGuard } from '../..//auth/auth.guard'
+import { UserOptionalAuthGuard } from '../../auth/auth.guard'
 
 @Controller()
 export class PublicController {
@@ -27,5 +35,48 @@ export class PublicController {
     }
 
     return res.render(`${data.root_theme as string}/index`, pageData)
+  }
+
+  @UseGuards(UserOptionalAuthGuard)
+  @Get('*')
+  async getPrivacyPolicy (@Request() req, @Res() res: Response): Promise<any> {
+    const name: string = req.params[0]
+
+    // try to find a md file
+    let mdFile
+    try {
+      mdFile = readFileSync(join(__dirname, '..', '..', '..', 'pages', `${name}.md`), 'utf8')
+    } catch (e) {
+      throw new NotFoundException()
+    }
+
+    // extract variables from md header, e.g. title
+    const mdParts = mdFile.split(/---\n/)
+    let mdVars = {}
+    let mdBody = mdFile
+    if (mdParts.length === 3) {
+      try {
+        mdVars = yaml.load(mdParts[1])
+        mdBody = mdParts[2]
+      } catch (_) {
+        mdBody = mdFile
+      }
+    }
+
+    // regular Saasform rendering variables
+    const data = await this.settingsService.getWebsiteRenderingVariables()
+    const pageData = {
+      ...data
+    }
+
+    const md = new MarkdownIt({ linkify: true })
+    const liquid = new Liquid()
+
+    // title from md header
+    pageData.page_title = mdVars.title ?? ''
+    // body first via liquid (to replace Saasform variable) then markdown-it
+    pageData.page_body = md.render(await liquid.parseAndRender(mdBody, pageData))
+
+    return res.render(`${data.root_theme as string}/page`, pageData)
   }
 }
