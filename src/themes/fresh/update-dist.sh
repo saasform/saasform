da4e60f83f1b859caa5c1d24291e07bb533086ea
Emanuele Cesena <emanuele.cesena@gmail.com>
2021-02-26 17:44:57 -0800

    Theme/fresh: billing page

--- src/themes/fresh/update-dist.sh
+++ src/themes/fresh/update-dist.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-rm -rf ../../webapp/themes/fresh
-rm -rf ../../webapp/themes/fresh-sso
 rm -rf ../../../data/themes/fresh
 rm -rf ../../../data/themes/fresh-sso
 
-cp -r dist ../../webapp/themes/fresh
-rm ../../webapp/themes/fresh/index-sso.liquid
-
 cp -r dist ../../../data/themes/fresh
 rm ../../../data/themes/fresh/index-sso.liquid
-
-cp -r dist ../../webapp/themes/fresh-sso
-rm -rf ../../webapp/themes/fresh-sso/assets/img
-mv ../../webapp/themes/fresh-sso/index-sso.liquid ../../webapp/themes/fresh-sso/index.liquid
-cp ../../webapp/themes/fresh-sso/index.liquid ../../webapp/themes/fresh-sso/login.liquid
 
 cp -r dist ../../../data/themes/fresh-sso
 rm -rf ../../../data/themes/fresh-sso/assets/img
