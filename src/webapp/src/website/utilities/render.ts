5fdde74b133fa93ea576689630325ebe186f0fae
Emanuele Cesena <emanuele.cesena@gmail.com>
2021-02-28 09:05:30 -0800

    Add renderUserPage with common logic

--- src/webapp/src/website/utilities/render.ts
+++ src/webapp/src/website/utilities/render.ts
@@ -20,4 +20,19 @@ const renderPage = (req, res, page: string, data = {}): Response => {
   return res.render(`${siteData.root_theme as string}/${page}`, pageData)
 }
 
-export { renderPage }
+/**
+ * Render a subpage of /user
+ * @param req The HTTP request
+ * @param res The HTTP response
+ * @param page A string representing the name of the template to display
+ * @param data An optional object containing the page-specific data to pass to the template
+ */
+const renderUserPage = (req, res, page: string, data = {}): Response => {
+  const userPageData = {
+    user_page: page,
+    ...data,
+  }
+  return renderPage(req, res, 'user', userPageData)
+}
+
+export { renderPage, renderUserPage }
