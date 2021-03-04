420777844d96c858ec5639f0b44da61a04286e46
Davide Vernizzi <davide@vernizzis.it>
2021-03-04 23:30:45 +0100

    fix: Render users in team page

--- src/webapp/src/website/utilities/render.ts
+++ src/webapp/src/website/utilities/render.ts
@@ -32,7 +32,9 @@ const renderUserPage = (req, res, page: string, data = {} as any): Response => {
   const userPageData = {
     user_page: page,
     // important - we must guarantee that payment_methods is an array
-    payment_methods: data?.account?.payments_methods ?? []
+    payment_methods: data?.account?.payments_methods ?? [],
+    account_users: data?.account_users ?? []
+
   }
   return renderPage(req, res, 'user', userPageData)
 }
