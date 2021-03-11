80bea496a57956506a10f2a81c21b5f7db3adccb
Davide Vernizzi <davide@vernizzis.it>
2021-03-11 11:29:48 +0100

    feat: add the flask demo

---
+++ src/demo-flask/src/main.py
@@ -0,0 +1,62 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Begin Saasform code
+import requests
+import os
+import json
+from flask_login import LoginManager, login_required, UserMixin, current_user
+import jwt
+
+# The user required by flask-login must have a set of properties.
+# You can inherit from UserMixin to have all the required properies.
+class SaasformUser(UserMixin):
+    account_id = 0
+    email = ''
+
+
+# Fetch public key from Saasform. You can also pass this from config file
+uri = os.environ['SAASFORM_SERVER'] + '/api/v1/public-key'
+pubkey = requests.get(uri).json()['message']
+
+
+# Creating login manager
+login_manager = LoginManager()
+
+# Custom handler to parse Saasform response
+@login_manager.request_loader
+def load_user_from_request(request):
+    app.logger.debug(request)
+
+    token = request.cookies.get('__session')
+    if token:
+        # validating JWT
+        try:
+            data = jwt.decode(token, pubkey, algorithms=['ES256'])
+        except:
+            app.logger.error('JWT validation failed')
+            return None
+
+        # flask-login requires a user to be returned.
+        # Here we can also replicate the user on out local DB to locally have the list of users
+        user = SaasformUser()
+        user.email = data.get('email', '')
+        user.account_id = data.get('account_id', 0)
+
+        return user
+    
+    # Toekn not found, user not autheticated
+    return None
+
+login_manager.init_app(app)
+# End Saasform code
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/protected")
+@login_required
+def protected():
+    return "Hello, " + current_user.email
