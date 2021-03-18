6c11178d9f5ec632b01026a17309496f6b30873a
Emanuele Cesena <emanuele.cesena@gmail.com>
2021-03-17 19:02:47 -0700

    Add placeholder for admin dasboard

--- src/webapp/src/website/controllers/admin.controller.ts
+++ src/webapp/src/website/controllers/admin.controller.ts
@@ -5,7 +5,6 @@ import {
   Res,
   UseGuards,
   NotFoundException,
-  InternalServerErrorException,
   Param
 } from '@nestjs/common'
 import { Response } from 'express'
@@ -29,27 +28,59 @@ export class AdminController {
   ) {}
 
   @Get('emails?')
-  getAllEmail (@Request() req, /*@Res() res: Response,*/): string {
-    let result: Array<string> = [];
+  getAllEmail (@Request() req /* @Res() res: Response, */): string {
+    const result: string[] = []
     readdirSync('./emails').forEach(file => {
       if (file.endsWith('.md')) {
         const name = file.replace('.md', '')
         result.push(`<a href="/admin/email/${name}">${name}</a>`)
       }
-    });
-    return result.join('<br/>');
+    })
+    return result.join('<br/>')
   }
 
   @Get('emails?/:email')
-  getEmail (@Request() req, /*@Res() res: Response,*/ @Param('email') email: string): string {
-    return `email: ${email}`;
+  async getEmail (@Request() req, @Res() res: Response, @Param('email') email: string): Promise<any> {
+    const name: string = email
+
+    // try to find a md file
+    let mdFile
+    try {
+      mdFile = readFileSync(join(__dirname, '..', '..', '..', 'emails', `${name}.md`), 'utf8')
+    } catch (e) {
+      throw new NotFoundException()
+    }
+
+    // extract variables from md header, e.g. title
+    const mdParts = mdFile.split(/---\n/)
+    let mdVars = { title: '' }
+    let mdBody = mdFile
+    if (mdParts.length === 3) {
+      try {
+        mdVars = yaml.load(mdParts[1])
+        mdBody = mdParts[2]
+      } catch (_) {
+        mdBody = mdFile
+      }
+    }
+
+    const md = new MarkdownIt({ linkify: true })
+    const liquid = new Liquid()
+
+    const pageData = {
+      // title from md header
+      page_title: mdVars.title ?? '',
+      // body first via liquid (to replace Saasform variable) then markdown-it
+      page_body: md.render(await liquid.parseAndRender(mdBody, req.websiteData))
+    }
+
+    return renderPage(req, res, 'page', pageData)
   }
 
   @Get('*')
-  getAdmin(@Res() res: Response) {
-    return 'Admin dashboard coming soon!'
-    // return res.sendFile('index.html', {
-    //   root: join(__dirname, '..', '..', 'admin'),
-    // });
+  getAdmin (@Res() res: Response): void {
+    return res.sendFile('index.html', {
+      root: join(__dirname, '..', '..', '..', 'admin')
+    })
   }
 }
