a5144b32251328464fff91a742e20a369705955e
Davide Vernizzi <davide@vernizzis.it>
2021-04-03 00:34:32 +0200

    feat: Send email when trial is about to expire
    Closes #74

---
+++ src/webapp/doc/design_docs/74_SubscripionExpirationEmail.md
@@ -0,0 +1,42 @@
+# Send email to notify that subscription is about to expire
+
+Issues:
+
+- [74](https://github.com/saasform/saasform/issues/74)
+
+As a User
+I want to receive an email few days before trial expires
+So that I can buy a subscription
+
+Make sure:
+
+- expiring trial subscriptions get a reminder email
+- expiring non trial subscription get no email **Q1?**
+- email is not sent twice per a subscription
+
+## Design process
+
+The idea is to send an email just before a subscription expires.
+
+- **Q1?** Only trial subscriptions or any subscription? [probably only trial]
+- **Q2?** Per subscription or per account?
+    Per subscription allows to have more than one email per account, if this makes any sense [probably not]
+
+A possible implementation would:
+
+- find all the expiring subscriptions
+- for each one, send an email
+
+How to efficiently find all the expiring subscriptions?
+
+1. If only trials a first filter can be on the subscription type
+2. In addition it's possible to add a field with the explicit value of the subscription expiration
+
+How to avoid sending more than once an email?
+
+1. Compute the exact time of sending (e.g. 24 hours before the expiration) and send the email only if the expiration time is after that moment, but not too much (e.g. 24 hours ago > expiration > 23 hours ago).
+  This allows to not add any additional fields, but it may lead to duplicate emails or to missing emails
+2. Add a field on subscription (or account) table (or json data) and check if already sent. This is more complex and feels like adding some data that does belong to the model (i.e. the notification)
+3. Add a notification table that keeps tracks of the notification sent [probably the better, but take more time]
+
+Probably 3. is better, but we can start with 1.
--- src/webapp/src/api/controllers/v1/api.payment.controller.ts
+++ src/webapp/src/api/controllers/v1/api.payment.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, UseGuards, Request, Res } from '@nestjs/common'
+import { Controller, Get, Post, UseGuards, Request, Res } from '@nestjs/common'
 import { Response } from 'express'
 import { SettingsService } from '../../../settings/settings.service'
 import { AccountsService } from '../../../accounts/services/accounts.service'
 import { PlansService } from '../../../payments/services/plans.service'
+import { PaymentsService } from '../../../payments/services/payments.service'
+import { NotificationsService } from '../../../notifications/notifications.service'
 
 import { UserRequiredAuthGuard } from '../../../auth/auth.guard'
 
@@ -11,7 +13,9 @@ export class ApiV1PaymentController {
   constructor (
     private readonly settingsService: SettingsService,
     private readonly accountsService: AccountsService,
-    private readonly plansService: PlansService
+    private readonly plansService: PlansService,
+    private readonly paymentsService: PaymentsService,
+    private readonly notificationService: NotificationsService
   ) {}
 
   @UseGuards(UserRequiredAuthGuard)
@@ -31,6 +35,26 @@ export class ApiV1PaymentController {
     return res.json({
       statusCode: 200,
       message: methods
+    })
+  }
+
+  // @UseGuards(UserRequiredAuthGuard)
+  @Get('send-trial-expiration')
+  async sendEmailReminder (@Request() req, @Res() res: Response): Promise<any> {
+    const expiringSubscriptions = await this.paymentsService.getExpiringPayments()
+    if (expiringSubscriptions != null) {
+      for (let i = 0; i < expiringSubscriptions.length; i++) {
+        const owner = await this.accountsService.getOwner(expiringSubscriptions[i].account_id)
+
+        if (owner != null) {
+          await this.notificationService.sendEmail(owner?.email, 'trial_expiring', {})
+        }
+      }
+    }
+
+    return res.json({
+      statusCode: 200,
+      message: 'done'
     })
   }
 }
