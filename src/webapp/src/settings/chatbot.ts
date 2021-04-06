24856f9f03f7a6dde41b80dec3b91a273fbe0c1e
Emanuele Cesena <emanuele.cesena@gmail.com>
2021-04-05 19:30:31 -0700

    Chatbot: add Intercom

--- src/webapp/src/settings/chatbot.ts
+++ src/webapp/src/settings/chatbot.ts
@@ -33,6 +33,47 @@ export const renderChatbotJs = (provider: string, id: string, req): string => {
         ]
       })
       return `<script type="text/javascript" id="hs-script-loader" async defer src="https://js.hs-scripts.com/${id}.js"></script>`
+    case 'intercom':
+      // 2021-04-05 https://www.intercom.com/help/en/articles/3894-using-intercom-with-content-security-policy
+      req.customCsp.push({
+        scriptSrc: [
+          // "'strict-dynamic'",
+          "'unsafe-inline'",
+          'https://app.intercom.io',
+          'https://widget.intercom.io',
+          'https://js.intercomcdn.com'
+        ],
+        connectSrc: [
+          'https://api.intercom.io',
+          'https://api-iam.intercom.io',
+          'https://api-ping.intercom.io',
+          'https://nexus-websocket-a.intercom.io',
+          'https://nexus-websocket-b.intercom.io',
+          'wss://nexus-websocket-a.intercom.io',
+          'wss://nexus-websocket-b.intercom.io',
+          'https://uploads.intercomcdn.com',
+          'https://uploads.intercomusercontent.com'
+        ],
+        fontSrc: [
+          'https://js.intercomcdn.com'
+        ],
+        imgSrc: [
+          'https://js.intercomcdn.com',
+          'https://static.intercomassets.com',
+          'https://downloads.intercomcdn.com',
+          'https://uploads.intercomusercontent.com',
+          'https://gifs.intercomcdn.com',
+          'https://video-messages.intercomcdn.com',
+          'https://messenger-apps.intercom.io',
+          'https://*.intercom-attachments-5.com',
+          'https://*.intercom-attachments-6.com',
+          'https://*.intercom-attachments-9.com'
+        ],
+        mediaSrc: [
+          'https://js.intercomcdn.com'
+        ]
+      })
+      return `<script>window.intercomSettings={app_id:"${id}"};(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/${id}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>`
   }
   return ''
 }
