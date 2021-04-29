7c9aa029a77ecbff6db77b59489882fc99270bae
Emanuele Cesena <emanuele.cesena@gmail.com>
2021-04-28 18:41:54 -0700

    Reporting: minor refactor and fix linter

--- src/webapp/src/utilities/reporting.ts
+++ src/webapp/src/utilities/reporting.ts
@@ -2,23 +2,22 @@ import * as uuid from 'uuid'
 
 import Reporter, { HumbugConsent } from '@bugout/humbug'
 
-const HUMBUG_TOKEN = '3bafba59-f078-4004-ab12-fa27cf4885f1'
+const HUMBUG_TOKEN = process.env.HUMBUG_TOKEN
 
-const packagePath = process.cwd()
-const packageConfigFile = require(`${packagePath}/package.json`)
-export let tags: string[] = [packageConfigFile.version]
+const version = process.env.npm_package_version ?? ''
+export const tags: string[] = [version]
 
 const sessionId = uuid.v4()
-let clientId = process.env['SAASFORM_REPORTER_CLIENT_ID']
+let clientId = process.env.SAASFORM_REPORTER_CLIENT_ID
 if (clientId === undefined) {
-	clientId = uuid.v4()
+  clientId = uuid.v4()
 }
 
-const saasform_consent = new HumbugConsent(true)
-export const saasform_reporter = new Reporter(
-	'saasform',
-	saasform_consent,
-	clientId,
-	sessionId,
-	HUMBUG_TOKEN
+const consent = new HumbugConsent(HUMBUG_TOKEN !== undefined)
+export const saasformReporter = new Reporter(
+  'saasform',
+  consent,
+  clientId,
+  sessionId,
+  HUMBUG_TOKEN
 )
