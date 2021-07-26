8879b270b1ac53e04581074e2023542887b50a34
Davide Vernizzi <davide@vernizzis.it>
2021-07-26 15:44:27 +0200

    test: Add e2e test for forced payment

--- src/e2e-test/features/steps/given.js
+++ src/e2e-test/features/steps/given.js
@@ -1,6 +1,5 @@
-const { Given } = require("cucumber");
+const { Given, When } = require("cucumber");
 const { createConnection, getManager } = require("typeorm");
-
 
 Given('admin sets settings {string} to {string}', async function (key, v) {
   let val
@@ -16,3 +15,14 @@ Given('admin sets settings {string} to {string}', async function (key, v) {
   await getManager().query(sql);
   c.close();
 });
+
+When(
+  'I set the card information {string} {string} on {string}', async function (number, extra, selector) {
+    const page = this.page
+    await page.click(selector)
+    await page.keyboard.type(number, { delay: 50 })
+    // await sleep(2000) // wait for post-credit card number sliding CardElement animation
+    await page.keyboard.type(extra, { delay: 50 })
+  }
+)
+
