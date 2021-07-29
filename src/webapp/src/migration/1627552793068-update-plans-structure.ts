43d5981b10f82426537d4fba76a6090024a097eb
Emanuele Cesena <emanuele.cesena@gmail.com>
2021-07-29 15:16:39 +0200

    Fix e2e tests

--- src/webapp/src/migration/1627552793068-update-plans-structure.ts
+++ src/webapp/src/migration/1627552793068-update-plans-structure.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class updatePlansStructure1627552793068 implements MigrationInterface {
   public async up (queryRunner: QueryRunner): Promise<void> {
